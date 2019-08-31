c3f464a641538f213c62006ca211960235dc2327
Brad <brad@brad.tw>
2019-08-31 12:07:55 +0800

    2

--- app/src/main/java/tw/org/iii/appps/brad40/MainActivity.java
+++ app/src/main/java/tw/org/iii/appps/brad40/MainActivity.java
@@ -1,28 +1,41 @@
 package tw.org.iii.appps.brad40;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private Vibrator vibrator;
     private SwitchCompat fswitch;
     private CameraManager cameraManager;
+    private ImageView img;
+    private File sdroot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +43,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA)
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
+                    new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    },
                     12);
         }else{
             init();
         }
 
-
+        img = findViewById(R.id.img);
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         fswitch = findViewById(R.id.fswitch);
         fswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -64,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void init(){
         cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
-
+        sdroot = Environment.getExternalStorageDirectory();
     }
 
     private void onFlashLight(){
@@ -101,6 +117,46 @@ public class MainActivity extends AppCompatActivity {
             vibrator.vibrate(pattern, -1);
         }else{
             vibrator.vibrate(pattern, 0);
+        }
+    }
+
+    public void test3(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivityForResult(intent, 3);
+    }
+
+    public void test4(View view){
+
+        Uri photoURI = FileProvider.getUriForFile(
+                this,
+                getPackageName() + ".provider",
+                new File(sdroot, "iii.jpg"));
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(intent, 4);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK ){
+            if (requestCode == 3){
+                Bundle bundle = data.getExtras();
+                Set<String> keys = bundle.keySet();
+                for (String key: keys){
+                    Log.v("brad", "key = " + key);
+                    Object obj = bundle.get(key);
+                    Log.v("brad", obj.getClass().getName());
+                }
+
+
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                img.setImageBitmap(bmp);
+            }
         }
     }
 }
